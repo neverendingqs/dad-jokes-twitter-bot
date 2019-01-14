ce74d906f68a0a88038188ae87365e2037700342
Mark Tse <mark.tse@neverendingqs.com>
2019-01-14 16:57:44 -0500

    app - now shows root path when at root path.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 exports.http = (request, response) => {
-  response.status(200).send(request.path);
+  response.status(200).send(request.path || '/');
 };
 
 exports.event = (event, callback) => {
